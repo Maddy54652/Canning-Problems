db6dae44efe86a71d207f07281ef9885913ed035
Maddy54652 <37761862+Maddy54652@users.noreply.github.com>
2018-08-01 14:19:50 -0700

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,15 @@
+/************************************************/
+/* Programmer: Madalyn Henderson                */
+/*                                              */
+/* Program 1: Hellow World and a Bit More       */
+/*                                              */
+/* Approximate completion time:   
+/************************************************/
+#include<stdio.h>
+
+int main(int argc, char* argv[]){
+	int value;
+	value = printf("Hello World!\n");
+	printf("The value given by the printf function is: %d.\n",value);
+	return 0;
+}
