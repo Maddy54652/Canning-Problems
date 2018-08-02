28f69d95a510974b723954d8a0caf3445e411ec5
Maddy54652 <37761862+Maddy54652@users.noreply.github.com>
2018-08-02 16:11:43 -0700

    Create problem1.c

---
+++ 100Problems1/problem1.c
@@ -0,0 +1,14 @@
+/************************************************/
+/* Programmer: Madalyn Henderson                */
+/*                                              */
+/* Program 1: Hellow World and a Bit More       */
+/*                                              */
+/* Approximate completion time:   3 minutes     */
+/************************************************/
+
+#include<stdio.h>
+
+int main(int argc,char* argv[]){
+  printf("Hello World!\n");
+  return 0;
+}
