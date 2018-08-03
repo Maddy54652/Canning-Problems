66805fd03fcf451eabcafd2d467f089c1d7071f3
Maddy54652 <37761862+Maddy54652@users.noreply.github.com>
2018-08-03 15:15:20 -0700

    Create Problem3.c

---
+++ 100Problems1/Problem3.c
@@ -0,0 +1,15 @@
+
+/************************************************/
+/* Programmer: Madalyn Henderson                */
+/*                                              */
+/* Program 3: The Character P                   */
+/*                                              */
+/* Approximate completion time:   4 minutes     */
+/************************************************/
+
+#include<stdio.h>
+
+int main(int argc,char* argv[]){
+	printf("%c\n",'P');
+	return 0;
+}
