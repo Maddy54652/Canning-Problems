fc0334fcaaf6ebf1785f8d01b2f178c9715d3842
Maddy54652 <37761862+Maddy54652@users.noreply.github.com>
2018-08-03 15:20:55 -0700

    Create problem4.c

---
+++ 100Problems1/problem4.c
@@ -0,0 +1,16 @@
+/************************************************/
+/* Programmer: Madalyn Henderson                */
+/*                                              */
+/* Program 4: The scanf Function                */
+/*                                              */
+/* Approximate completion time:   6 minutes     */
+/************************************************/
+
+#include<stdio.h>
+
+int main(int argc, char* argv[]){
+	int input;
+	scanf("%d",&input);
+	printf("%d\n",input);
+	return 0;
+}
