7a98bb0603df4b4a43d13040470cd44e6e4bde6b
Maddy54652 <37761862+Maddy54652@users.noreply.github.com>
2018-08-03 15:08:31 -0700

    Create problem2.c

---
+++ 100Problems1/problem2.c
@@ -0,0 +1,15 @@
+
+/************************************************/
+/* Programmer: Madalyn Henderson                */
+/*                                              */
+/* Program 2: The Value 6                       */
+/*                                              */
+/* Approximate completion time:   5 minutes     */
+/************************************************/
+
+#include<stdio.h>
+
+int main(int argc,char* argv[]){
+  printf("%d\n",6);
+  return 0;
+}
