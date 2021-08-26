fa8d5a798d0dce30eafa8696d60ebea2eff53f0c
Maddy <37761862+Maddy54652@users.noreply.github.com>
2021-08-26 16:35:44 -0700

    Create problem13.c

---
+++ 100Problems1/problem13.c
@@ -0,0 +1,28 @@
+/************************************************/
+/* Programmer: Madalyn Henderson                */
+/*                                              */
+/* Program 13: 	The abs Function	        */
+/*                                              */
+/* Approximate completion time: 4 minutes       */
+/************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char* argv[]){
+	
+	int input;
+
+	scanf("%d",&input);
+	input = abs(input);
+	printf("%d\n",input);
+
+	return 0;
+}
+
+//What was the trouble in River City?
+//They put a new pool table in the billiard parlor
+//and Billiards is a gateway to trouble
+//with a capital T
+//and that rhymes with P
+//and that stands for POOL
