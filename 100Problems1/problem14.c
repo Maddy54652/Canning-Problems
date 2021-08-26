c214b60e3c02b613414bcb70851b3a2298c2b8da
Maddy <37761862+Maddy54652@users.noreply.github.com>
2021-08-26 16:41:21 -0700

    Create problem14.c

---
+++ 100Problems1/problem14.c
@@ -0,0 +1,17 @@
+/************************************************/
+/* Programmer: Madalyn Henderson                */
+/*                                              */
+/* Program 14: Argc	                 		        */
+/*                                              */
+/* Approximate completion time: 1 minute        */
+/************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char* argv[]){
+	
+	printf("%d\n",argc);
+
+	return 0;
+}
